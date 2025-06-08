f26c26a48d9bb49c0b35cb1d546400b61930f6d4
Berat Hündürel <berat.hundurel@bilgiedu.net>
2025-06-08 21:57:31 +0300

    Add WASM build, web assets, and Windows resources
    - Enable fullscreen and hide cursor in main.go
    - Add WASM build and HTML loader for web deployment
    - Include server for serving WASM files
    - Add Windows icon and manifest resources

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func main() {
 	ebiten.SetWindowTitle("Go Asteroids")
 	ebiten.SetWindowSize(goasteroids.ScreenWidth, goasteroids.ScreenHeight)
 
+	ebiten.SetCursorMode(ebiten.CursorModeHidden)
+	ebiten.SetFullscreen(true)
+
 	if err := ebiten.RunGame(&goasteroids.Game{}); err != nil {
 		log.Fatal(err)
 	}
